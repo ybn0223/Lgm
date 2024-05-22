abdf32875b39604cc5f43f1094146bb859257acb
Yassine B <yassinebenmendour2@gmail.com>
2024-05-22 14:28:49 +0200

    changed port for render

--- index.ts
+++ index.ts
@@ -14,7 +14,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, "views"));
 
-app.set("port", process.env.PORT || 3000);
+app.set("port", process.env.PORT || 10000);
 
 app.get("/", (req, res) => {
     res.render("index")
